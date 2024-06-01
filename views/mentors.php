1c11dd010345cdd4cfe4f24145900741039e994e
RajeevG187 <rajeevg@iitbhilai.ac.in>
2024-06-01 17:04:07 +0530

    rebased

--- views/mentors.php
+++ views/mentors.php
@@ -78,8 +78,13 @@ if (json_last_error() !== JSON_ERROR_NONE) {
                 <h2 class="heading-secondary"><?= $data['coordinators']['heading'] ?></h2>
             </div>
 
+<<<<<<< HEAD
             <!-- Render faculty members 2 membered -->
             <?php foreach ($data['coordinators']['faculty-2-membered'] as $members): ?>
+=======
+            <!-- Render faculty members -->
+            <?php foreach ($data['coordinators']['faculty'] as $members): ?>
+>>>>>>> 4c0fb5a (Integrated Json in mentors.php file and updated all mentors and Coordinators data, added linked in profile option, updated all images on mentors page.)
                 <div class="row">
                     <div class="dev">
                         <?php foreach ($members as $member): ?>
@@ -112,6 +117,7 @@ if (json_last_error() !== JSON_ERROR_NONE) {
                     </div>
                 </div>
             <?php endforeach; ?>
+<<<<<<< HEAD
 
             <!-- Render faculty members 3 membered -->
             <?php foreach ($data['coordinators']['faculty-3-membered'] as $members): ?>
@@ -147,6 +153,8 @@ if (json_last_error() !== JSON_ERROR_NONE) {
                     </div>
                 </div>
             <?php endforeach; ?>
+=======
+>>>>>>> 4c0fb5a (Integrated Json in mentors.php file and updated all mentors and Coordinators data, added linked in profile option, updated all images on mentors page.)
             
             <!-- Render mentor coordinators -->
             <?php foreach ($data['coordinators']['mentor-coordinators'] as $members): ?>
