3ba77804a234f887eb1447998e66a81184322bde
Amay Dixit <amaydixit11@gmail.com>
2024-06-01 19:12:49 +0530

    modified:   src/json/meraz.json 	modified:   views/meraz.php

--- views/meraz.php
+++ views/meraz.php
@@ -1,6 +1,26 @@
+<?php
+// Define the path to the JSON file
+$jsonFilePath = '../src/json/meraz.json';
+
+// Check if the JSON file exists and is readable
+if (!file_exists($jsonFilePath) || !is_readable($jsonFilePath)) {
+    die('Error: JSON file not found or not readable.');
+}
+
+// Read JSON file
+$json = file_get_contents($jsonFilePath);
+
+// Decode JSON data to PHP associative array
+$data = json_decode($json, true);
+
+// Check if JSON data is valid
+if (json_last_error() !== JSON_ERROR_NONE) {
+    die('Error: Invalid JSON data. ' . json_last_error_msg());
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
-
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -19,17 +39,17 @@
 
         <meta name="google-site-verification" content="psQtXO_U0R9o5w-fP7i3zrlm_g3nDtP0Mg6-Xg-q73w" />
         <!-- Global site tag (gtag.js) - Google Analytics -->
-		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-121551593-1"></script>
-		<script>
-		  window.dataLayer = window.dataLayer || [];
-		  function gtag(){dataLayer.push(arguments);}
-		  gtag('js', new Date());
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-121551593-1"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
 
-		  gtag('config', 'UA-121551593-1');
-		</script>
+          gtag('config', 'UA-121551593-1');
+        </script>
 
     </head>
-      <style>
+    <style>
       .no-cssanimations .rw-wrapper .rw-sentence span:first-child{
         opacity: 1;
       }
@@ -40,85 +60,52 @@
       require_once('partials/header.php');
       ?>
 
-        <header class="header header__meraz">
-            <div class="container">
-                <div class="header__hero-box">
-                    <h1 class="heading-primary" style="font-family: 'Bangers'; letter-spacing: 5px;font-size:900%;">Meraz '<div class="rw-words rw-words-1" style="letter-spacing: 10px;">
-                      <span>18</span>
-                      <span>19</span>
-                      <span>22</span>
-                      <span>18</span>
-                      <span>19</span>
-                      <span>22</span>
-                      
-                    </div></h1>
-                    <p class="heading-subtitle">Techno-Cultural Fest of IIT Bhilai</p>
-                </div>
-            </div>
-        </header>
-        <section class="section">
+      <header class="header header__meraz">
           <div class="container">
-            <center>
-              <h2 class="heading-secondary">Meraz '22 Trailer</h2>
-            <iframe width="840" height="472.5" src="https://www.youtube.com/embed/iDtwPVqDfkA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-            </center><br><br>
-              <a href="https://meraz.in" class="outreach__link"><p class="introduction__text" style="text-align:center;">Go to - <u>Meraz'22 Official Website</u></p></a><br>
-            </div> 
-          <div class="container">
-            <center>
-              <h2 class="heading-secondary">Meraz '19 Trailer</h2>
-            <iframe width="840" height="472.5" src="https://www.youtube.com/embed/vrHzl-E-Pvs" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            </center><br><br>
-              <a href="https://meraz.in" class="outreach__link"><p class="introduction__text" style="text-align:center;">Go to - <u>Meraz'19 Official Website</u></p></a><br>
+              <div class="header__hero-box">
+                  <h1 class="heading-primary" style="font-family: 'Bangers'; letter-spacing: 5px;font-size:900%;"><?php echo $data["header"]["heading"]; ?></h1>
+                  <p class="heading-subtitle"><?php echo $data["header"]["subtitle"]; ?>.</p>
+              </div>
           </div>
+      </header>
 
-            <div class="container">
-              <center>
-                <h2 class="heading-secondary">Meraz '18 Aftermovie</h2>
-                <iframe width="840" height="472.5" src="https://www.youtube.com/embed/uOQbTb4-ejs" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-              </center><br><br>
-                <a href="http://meraz.iitbhilai.ac.in" class="outreach__link"><p class="introduction__text" style="text-align:center;">Go to - <u>Meraz'18 Official Website</u></p></a><br>
-        </div>
+      <section class="section">
+        <?php foreach ($data["meraz-videos"]['info'] as $item): ?>
+          <div class="container" style = "text-align: center;" >
+              <h2 class="heading-secondary"><?php echo $item['title']; ?></h2>
+              <iframe width="840" height="472.5" src=<?php echo $item['link']; ?> title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <br><br>
+              <a href=<?php echo $item['site-link']; ?> class="outreach__link"><p class="introduction__text" style="text-align:center;">Go to - <u><?php echo $item["site-name"]; ?></u></p></a><br>
+          </div> 
+        <?php endforeach;?>
+
         <br>
+        
         <div class="container">
-            <h2 class="heading-secondary">More Videos</h2>
+            <h2 class="heading-secondary"><?php echo $data["more-videos-meraz"]['heading'] ?></h2>
+            <!-- <h2 class="heading-secondary">More Videos</h2> -->
         </div>
-        <div class="row">
-            <div class="col-1-of-2">
-                <div class="campus">
-                  <iframe width="560" height="315" src="https://www.youtube.com/embed/gKt4JCaQlmA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                </div>
-            </div>
-            <div class="col-1-of-2">
-                <div class="campus">
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/2fMa3wJy0H0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-           <!-- meraz'19 aftermovie  -->
-            <div class="col-1-of-2">
-                <div class="campus">
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/fjtuMhY1KK8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                </div>
-            </div>
 
-            <!-- meraz'22 teaser -->
-            <div class="col-1-of-2">
-                <div class="campus">
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/W0VIHUbZ680" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                </div>
-            </div>
+        <?php foreach ($data["more-videos-meraz"]['youtube-links'] as $item): ?>
+          <div class="row">
+              <div class="col-1-of-2">
+                  <div class="campus">
+                    <iframe width="560" height="315" src=<?php echo $item[0]["link"]; ?> frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                  </div>
+              </div>
+              <div class="col-1-of-2">
+                  <div class="campus">
+                      <iframe width="560" height="315" src=<?php echo $item[1]["link"]; ?> frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                  </div>
+              </div>
+          </div>
+        <?php endforeach;?>
+        
+      </section>
 
-        </div>
-        </section>
-
-
-
-
-        <?php
-        require_once('partials/footer.php');
-        ?>
+      <?php
+      require_once('partials/footer.php');
+      ?>
 
     </body>
 
