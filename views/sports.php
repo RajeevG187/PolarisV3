24161d6bbbee219eb8c0de9b138a26f14b8b1373
Jayant Jaiswal <jayantjaiswal.0802u@gmail.com>
2024-06-06 17:07:32 +0530

    updated the sports page

--- views/sports.php
+++ views/sports.php
@@ -1,3 +1,29 @@
+<?php
+// Define the path to the JSON file
+$jsonFilePath = '../src/json/sports.json';
+
+// Check if the JSON file exists and is readable
+if (!file_exists($jsonFilePath) || !is_readable($jsonFilePath)) {
+    die('Error: JSON file not found or not readable.');
+}
+
+// Read JSON file
+$json = file_get_contents($jsonFilePath);
+
+// Decode JSON data to PHP associative array
+$data = json_decode($json, true);
+
+// Check if JSON data is valid
+if (json_last_error() !== JSON_ERROR_NONE) {
+    die('Error: Invalid JSON data. ' . json_last_error_msg());
+}
+
+// // Check if the required keys exist in the decoded data
+// if (!isset($data['header'], $data['introduction'], $data['topics'], $data['gallery'])) {
+//     die('Error: Missing required data in JSON.');
+// }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -49,141 +75,66 @@
 
         <section class="section">
             <div class="container">
-                <h2 class="heading-secondary">Sports Facilties</h2>
+                <h2 class="heading-secondary"><?php echo $data['header']['head']; ?></h2>
                 <div class="clubs">
-                    <p class="outreach__text"><i class="fas fa-arrow-circle-right"></i>&nbsp;<span>Follow us:</span> <a href="https://www.facebook.com/SportsIITBhilai/" class="outreach__link">Facebook</a></p>
+                    <p class="outreach__text"><i class="fas fa-arrow-circle-right"></i>&nbsp;<span>Follow us: </span><a href="<?php echo $data['header']['link']; ?>" class="outreach__link">Facebook</a></p>
                     <br>
-                    <h3 class="heading-tertiary">General Secretary - Lalit Gour</h3>
-                    <p class="clubs__text">Education &mdash; for many this is probably one of the most important factors of our lives and from a very young age children work their way through the education system before being able to make their own decisions about whether to continue their learning through college, university and further education. As part of our educational syllabus we are expected to learn about a number of subjects, however, one of particular interest to us is physical education and sports, as they have a massive part to play within the education system. Sports teach us key values such as: Teamwork, Respect, Passion, Hard work and many other key attributes that help towards developing an individual’s character. Presence of good sports facilities is a vital tool in developing the above skills. The following sections elaborate on the sports facilities present at IIT Bhilai which will help the students to tackle the challenging curriculum with renewed vigour. For indoor games there is a recreation room in both the hostels. It houses T.T., board games, pool and Television facilities.</p>
+                    <h3 class="heading-tertiary"><?php echo $data['introduction']['general-secretary']; ?></h3>
+                    <p class="clubs__text"><?php echo $data['introduction']['text']; ?></p>
                 </div>
 
+                <?php foreach ($data['sports'] as $sport): ?>
+                
                 <div class="sports">
-                    <h3 class="heading-tertiary">Cricket</h3>
-                    <p class="sports__text"><i class="fas fa-arrow-circle-right"></i>&nbsp;<span>Co-ordinator:</span> Amarjeet Kumar</p><br>
-                    <p class="sports__text"><i class="fas fa-arrow-circle-right"></i>&nbsp;<span>Facilities: </span>The club is provided with many facilities ranging from the cricket kits to the flood lights for the practice sessions. The club has a spectacular ground behind Academic Block. The ground has a well maintained pitch with a clean outfield. We are setting up cricket nets for practice in preparation for inter IIT sports meet and various other tournaments. </p><br>
 
-                    <p class="sports__text"><i class="fas fa-arrow-circle-right"></i>&nbsp;<span>Past Events: </span> Prayatna 2k22</p><br>
+                    <h3 class="heading-tertiary"><?php echo $sport['name']; ?></h3>
 
-                    <p class="sports__text"><i class="fas fa-arrow-circle-right"></i>&nbsp;<span>Upcoming Events: </span> Prayatna 2k23 , Various Tournaments in Raipur</p>
-                </div>
+                    <p class="sports__text"><i class="fas fa-arrow-circle-right"></i>&nbsp;<span><?php echo $sport['role']; ?></span><?php echo $sport['leader']; ?></p><br>
 
-                <div class="line-break"></div>
+                    <?php if (!empty($sport['about'])): ?>
+                        <p class="sports__text"><i class="fas fa-arrow-circle-right"></i>&nbsp;<span>About: </span><?php echo $sport['about']; ?></p><br>
+                    <?php endif; ?>
 
-                <div class="sports">
-                    <h3 class="heading-tertiary">Badminton</h3>
-                    <p class="sports__text"><i class="fas fa-arrow-circle-right"></i>&nbsp;<span>Co-ordinator:</span> Sanyam Sharma and Nukala Shreya</p><br>
-                    <p class="sports__text"><i class="fas fa-arrow-circle-right"></i>&nbsp;<span>Facilities: </span>We have four badminton courts and we are planning for renewal of the courts. Two of the courts are in Castle Ena and two are in Castle Dio. </p><br>
+                    <?php if (!empty($sport['facilities'])): ?>
+                        <p class="sports__text"><i class="fas fa-arrow-circle-right"></i>&nbsp;<span>Facilities: </span><?php echo $sport['facilities']; ?></p><br>
+                    <?php endif; ?>  
 
-                    <p class="sports__text"><i class="fas fa-arrow-circle-right"></i>&nbsp;<span>Objective: </span> Objective is to provide the new comers an environment where along the academics they will find that sports play a vital role in our life.</p><br>
+                    <?php if (!empty($sport['objective'])): ?>
+                        <p class="sports__text"><i class="fas fa-arrow-circle-right"></i>&nbsp;<span>Objective: </span><?php echo $sport['objective']; ?></p><br>
+                    <?php endif; ?>
 
-                    <p class="sports__text"><i class="fas fa-arrow-circle-right"></i>&nbsp;<span>Activities: </span> We have organised two badminton tournaments in last two years and we are planning for more tournaments in the upcoming years. Our main motive is to provide deserving students a better platform like INTER-IIT SPORTS MEET. INTER-IIT is the best platform for us where we can achieve our uniqueness in the sports between the other IITs. As we have already experienced the level of badminton in the INTER-IIT so we are planning to provide all the facilities to the interested students so that they can practice well and perform well in INTER-IIT.</p>
+                    <?php if (!empty($sport['activities'])): ?>
+                        <p class="sports__text"><i class="fas fa-arrow-circle-right"></i>&nbsp;<span>Activities: </span><?php echo $sport['activities']; ?></p><br>
+                    <?php endif; ?>
 
-                    <br>
+                    <?php if (!empty($sport['past-events'])): ?>
+                        <?php foreach ($sport['past-events'] as $pastevent): ?>    
+                            <p class="sports__text"><i class="fas fa-arrow-circle-right"></i>&nbsp;<span><?php echo $pastevent['head']; ?></span><?php echo $pastevent['text']; ?></p><br>
+                        <?php endforeach; ?>
+                    <?php endif; ?>
 
-                    <div class="row">
-                        <div class="col-1-of-2"><img src="../public/images/life/badminton-1.JPG" alt="Badminton" class="sports__photo"></div>
-                        <div class="col-1-of-2"><img src="../public/images/life/badminton-2.JPG" alt="Badminton" class="sports__photo"></div>
-                    </div>
+                    <?php if (!empty($sport['upcoming-events'])): ?>
+                        <?php foreach ($sport['upcoming-events'] as $upcomingevent): ?>
+                            <p class="sports__text"><i class="fas fa-arrow-circle-right"></i>&nbsp;<span><?php echo $upcomingevent['head']; ?></span><?php echo $upcomingevent['text']; ?></p><br>
+                        <?php endforeach; ?>
+                    <?php endif; ?>
 
-                </div>
-
-                <div class="line-break"></div>
-
-                <div class="sports">
-                    <h3 class="heading-tertiary">Basketball</h3>
-                    <p class="sports__text"><i class="fas fa-arrow-circle-right"></i>&nbsp;<span>Co-ordinator:</span> Vanshika and Karan Sunil Kumbhar</p><br>
-                    <p class="sports__text"><i class="fas fa-arrow-circle-right"></i>&nbsp;<span>Facilities: </span>There is an outdoor basketball court of standard dimensions with asphalt flooring open for everyone all round the day and night. We have our own Basketball coach available for training and support. </p><br>
-
-                    <p class="sports__text"><i class="fas fa-arrow-circle-right"></i>&nbsp;<span>Events Organised: </span>Basketball was one of the sports in the intra-IIT Sports League, Prayatna which is held annually in the winter semester. The Basketball team of IIT Bhilai participated in NITR's sports fest Samar, and secured 3rd position.</p><br>
-
-                    <p class="sports__text"><i class="fas fa-arrow-circle-right"></i>&nbsp;<span>Future Plans: </span> We are hopeful that with the team we have, we'll be able to participate in this year's Inter-IIT Sports Meet. Also, we are looking forward to participating in more competitions in and around the campus.</p>
-
-                    <br>
-
-                    <div class="row">
-                        <div class="col-1-of-2"><img src="../public/images/life/bb-1.JPG" alt="Basketball" class="sports__photo"></div>
-                        <div class="col-1-of-2"><img src="../public/images/life/bb-2.JPG" alt="Basketball" class="sports__photo"></div>
-                    </div>
-
+                    <?php if (!empty($sport['image'])): ?>
+                        <div class="row">
+                            <?php foreach ($sport['image'] as $images): ?>
+                                <div class="col-1-of-2"><img src="<?php echo $images['path']; ?>" alt="<?php echo $images['alt']; ?>" class="sports__photo"></div>
+                            <?php endforeach; ?>        
+                        </div>    
+                    <?php endif; ?>  
 
                 </div>
 
                 <div class="line-break"></div>
 
-                <div class="sports">
-                    <h3 class="heading-tertiary">Table Tennis</h3>
-                    <p class="sports__text"><i class="fas fa-arrow-circle-right"></i>&nbsp;<span>Co-ordinator:</span> Nishchay Rajput and Ishita Saxena</p><br>
-                    <p class="sports__text"><i class="fas fa-arrow-circle-right"></i>&nbsp;<span>About: </span>Table Tennis is a fast game that requires skills to win. This is the reason for its popularity all around the world. This year our first TT Boys team participated in INTER IIT Sports Meet 2017 in Madras. </p><br>
-
-                    <p class="sports__text"><i class="fas fa-arrow-circle-right"></i>&nbsp;<span>Facilities:</span> We have one TT Table in every Hostel. Sports Coaches and TT Coordinator are always available for any guidance and support. </p><br>
-
-                    <p class="sports__text"><i class="fas fa-arrow-circle-right"></i>&nbsp;<span>Events Organised: </span>An Intra IIT TT tournament is organised every semester, along with distributions of Trophy and Certificates. Table Tennis tournament is held along with various other sports in Intra IIT League "PRAYATNA".</p><br>
-
-                    <p class="sports__text"><i class="fas fa-arrow-circle-right"></i>&nbsp;<span>Future Plans: </span> We are hopeful that with the team we have, we'll be able to participate in this year's Inter-IIT Sports Meet. Also, we are looking forward to participating in more competitions in and around the campus.</p>
-
-                    <br>
-
-                    <div class="row">
-                        <div class="col-1-of-2"><img src="../public/images/life/tt-1.JPG" alt="Table Tennis" class="sports__photo"></div>
-                        <div class="col-1-of-2"><img src="../public/images/life/tt-2.JPG" alt="Table Tennis" class="sports__photo"></div>
-                    </div>
-
-
-                </div>
-
-                <div class="line-break"></div>
-
-                <div class="sports">
-                    <h3 class="heading-tertiary">Football</h3>
-                    <p class="sports__text"><i class="fas fa-arrow-circle-right"></i>&nbsp;<span>Co-ordinator:</span> Pravar Gupta</p><br>
-                    <p class="sports__text"><i class="fas fa-arrow-circle-right"></i>&nbsp;With almost of thirty regular members, the football club of IIT Bhilai is still growing. We have a standard size football ground and posts. Football came to the lime light during PRAYATNA which is held as a part of our sports curriculum where it became a huge success. Our passionate group of people is led by our coach, Mr. Sumit Tiwari who has a reputation for himself which stands undisputed. We have players who, given the opportunity have potential to prove themselves very valuable on the field. We are yet to represent ourselves in the Inter IIT Sports Meet but that is not an issue anymore, as from this year onwards we have a very capable team that can and will cause serious competition in the tournament. We have also planned to participate in the local football tournaments organised by other institutes and organisations that will contribute to our growth and evolve us into better players. </p>
-                </div>
-
-                <div class="line-break"></div>
-
-                <div class="sports">
-                    <h3 class="heading-tertiary">Chess Club</h3>
-                    <p class="sports__text"><i class="fas fa-arrow-circle-right"></i>&nbsp;<span>Co-ordinator:</span> Shivam Shrivastava</p><br>
-                    <p class="sports__text"><i class="fas fa-arrow-circle-right"></i>&nbsp;<span>About: </span>Chess is a combination of art, knowledge, game and risk. The complexity of chess is incomprehensible. And that’s what makes it beautiful, elegant and artistic. </p> <br>
-
-                    <p class="sports__text"><i class="fas fa-arrow-circle-right"></i>&nbsp;<span>Objective: </span>Our objective is to form a community of vibrant chess players. Also a strong chess team to represent IIT-Bhilai in inter-IITs and other national level tournaments. </p> <br>
-
-                    <p class="sports__text"><i class="fas fa-arrow-circle-right"></i>&nbsp;<span>Events Organised: </span>We hosted a chess tournament in our hostel, that triggered huge interest in chess among students. With that in mind we have introduced Graviton, a rapid chess tournament. All students, faculties and staffs are invited participate and exhibit their game skills. Graviton is a great platform to participate and improve one’s game.</p>
-
-                    <br>
-                    
-                    <div class="row">
-                        <div class="col-1-of-2"><img src="../public/images/life/chess-1.JPG" alt="Chess" class="sports__photo"></div>
-                        <div class="col-1-of-2"><img src="../public/images/life/chess-2.JPG" alt="Chess" class="sports__photo"></div>
-                    </div>
-
-
-                </div>
-
-                <div class="line-break"></div>
-
-                <div class="sports">
-                    <h3 class="heading-tertiary">Volleyball</h3>
-                    <p class="sports__text"><i class="fas fa-arrow-circle-right"></i>&nbsp;<span>Co-ordinator:</span> Ankadala Hemanth and Esha Jain</p><br>
-                    <p class="sports__text"><i class="fas fa-arrow-circle-right"></i>&nbsp;<span>About: </span>Our volleyball team was one of the first teams to participate in the inter iit sports meet organized by iit Madras. We have a great team and a wonderful captain Bapuji raj who lead us in inter iit sports meet. Our volleyball court is available 24x7 with sufficient lighting for night matches also.</p> <br>
-
-                    <p class="sports__text"><i class="fas fa-arrow-circle-right"></i>&nbsp;<span>Facilities: </span>We have a great coach ‘Sumit Tiwari’ who is friendly to interact with. We have proper equipment for practice.</p> <br>
-
-                    <p class="sports__text"><i class="fas fa-arrow-circle-right"></i>&nbsp;<span>Events Organised: </span>We have organized a intra tournament in first year, in which winners were awarded with prizes and certificate. In second year we have participated in inter iit sport meet and we have organized a league tournament “PRAYATNA” including volleyball, athletics, table tennis, basketball etc.</p> <br>
-
-                    <p class="sports__text"><i class="fas fa-arrow-circle-right"></i>&nbsp;<span>Future Plans: </span>We look forward to work with sports council to organize intra college volleyball tournament each semester. We will try to participate in inter college sport meets with other nearby colleges and also give our best in the upcoming inter iit sports meet organized by IIT Guwahati.</p> <br>
-
-                    <div class="row">
-                        <div class="col-1-of-2"><img src="../public/images/life/volley-1.JPG" alt="Volleyball" class="sports__photo"></div>
-                        <div class="col-1-of-2"><img src="../public/images/life/volley-2.JPG" alt="Volleyball" class="sports__photo"></div>
-                    </div>
-
-
-                </div>
+                <?php endforeach; ?>
 
             </div>
         </section>
-
 
         <?php
         require_once('partials/footer.php');
